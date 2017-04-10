e1a2287a599f2cd4554989c0866853df51ec3864
timsl <tiol@kth.se>
2017-04-10 20:10:54 +0200

    Each class has their own box-size

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,16 @@ import matplotlib.image as mpimg
 
 SeaLionCoord = namedtuple('SeaLionCoord', ['img_id', 'x', 'y', 'cls'])
 
+colors = (
+            (243,8,5),          # red
+            (244,8,242),        # magenta
+            (87,46,10),         # brown 
+            (25,56,176),        # blue
+            (38,174,21),        # green
+            )
+
+sizes = (45, 35, 25, 17, 25)
+
 def neighbourhood(xs, ys):
     return sum((x-y)**2 for x, y in zip(xs, ys)) > 100
 
@@ -23,19 +33,21 @@ def extract_points(original, dotted, img_id):
     x, y = np.where(np.any(diff > 30, axis=2))
     xy = zip(x, y)
     coords = remove_neighbourhood(xy, neighbourhood)
-    sealioncoords = [SeaLionCoord(img_id, x, y, dotted[x, y]) for x, y in coords]
+    sealioncoords = [SeaLionCoord(img_id, x, y, np.argmin(np.linalg.norm(colors -
+            dotted[x, y], axis = 1))) for x, y in coords]
     return sealioncoords
 
 def draw_boxes(img, coords):
     for i in coords:
-        img[i.x-10:i.x+10, i.y-10] = i.cls
-        img[i.x-10:i.x+10, i.y+10] = i.cls
-        img[i.x-10, i.y-10:i.y+10] = i.cls
-        img[i.x+10, i.y-10:i.y+10] = i.cls
+        size = sizes[i.cls]
+        img[i.x-size:i.x+size, i.y-size] = colors[i.cls]
+        img[i.x-size:i.x+size, i.y+size] = colors[i.cls]
+        img[i.x-size, i.y-size:i.y+size] = colors[i.cls]
+        img[i.x+size, i.y-size:i.y+size] = colors[i.cls]
     plt.imshow(img)
     plt.show()
 
-for i in range(41, 42):
+for i in range(44, 45):
     img = mpimg.imread("Train/%d.jpg" % i)
     dotted_img = mpimg.imread("TrainDotted/%d.jpg" % i)
     sealioncoords = extract_points(img, dotted_img, i)
