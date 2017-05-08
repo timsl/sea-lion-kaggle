ca95dd629795a1a452f9e3d8d2fc4933ee3f895d
fizzoo <vixirfizz@gmail.com>
2017-05-08 16:30:46 +0200

    Added RNG mode for visualization

--- picklevis.py
+++ picklevis.py
@@ -35,23 +35,27 @@ def main():
     args = sys.argv
 
     if argv <= 1:
-        print("Usage:", args[0], "'picklefile' 'offset/rng'")
+        print("Usage:", args[0], "'picklefile' 'offset/(negative for rng seed)'")
         exit(0)
 
-    rng = False
+    rng = None
+    offset = 0
 
-    if argv >= 3:
-        if args[2] == 'rng':
-            rng = True
+    if argv >= 2:
+        r = int(args[2])
+        if r < 0:
+            rng = -r
         else:
-            offset = int(args[2])
-    else:
-        offset = 0
+            offset = r
 
     fil = args[1]
     m = openp(fil)
 
-    print("Shape:")
+    print("Shape:", m.shape)
+
+    if rng is not None:
+        np.random.seed(rng)
+        np.random.shuffle(m)
 
     nplen = min(64, m.shape[0])
     cols = int(np.ceil(np.sqrt(nplen)))
