20c3c139c1c8a5e6774c1b2f4ddaad6ca8af1992
fizzoo <vixirfizz@gmail.com>
2017-05-11 21:52:35 +0200

    Default to 0 negatives

--- preprocess.py
+++ preprocess.py
@@ -169,7 +169,7 @@ def pickle_many(start, stop, file_prefix):
     p_target_imgs = np.array(target_imgs)
     p_counts = np.array(counts)
 
-    np_imgs, target_imgs, counts = remove_some_negative(p_np_imgs, p_target_imgs, p_counts, 1.0)
+    np_imgs, target_imgs, counts = remove_some_negative(p_np_imgs, p_target_imgs, p_counts, 0.0)
 
     pickle_save(np_imgs, file_prefix + "_x.p")
     pickle_save(target_imgs, file_prefix + "_y.p")
