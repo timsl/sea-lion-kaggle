74065ac865922c607ac5cf8476bfcb3600ab5971
fizzoo <vixirfizz@gmail.com>
2017-05-15 01:11:47 +0200

    'isort':ed imports

--- picklevis.py
+++ picklevis.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import pickle
+import sys
+
+import numpy as np
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
-import sys
 
 
 def openp(fil):
